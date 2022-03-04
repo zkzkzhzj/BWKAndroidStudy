57dff32ce5d6c587064d4625927297b6f9ae1864
Brain <zkzkzhzj>
2022-03-04 18:02:32 +0900

    finish Custom and stop watch

--- app/src/main/java/com/example/bwkandroidstudy/view/StopWatchActivity.kt
+++ app/src/main/java/com/example/bwkandroidstudy/view/StopWatchActivity.kt
@@ -3,10 +3,11 @@ package com.example.bwkandroidstudy.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.KeyEvent
 import android.widget.Toast
-import com.example.bwkandroidstudy.R
 import com.example.bwkandroidstudy.databinding.ActivityStopWatchBinding
+import java.util.prefs.Preferences
 
 class StopWatchActivity : AppCompatActivity() {
     lateinit var binding : ActivityStopWatchBinding
@@ -21,9 +22,12 @@ class StopWatchActivity : AppCompatActivity() {
 
         binding = ActivityStopWatchBinding.inflate(layoutInflater)
 
+        Log.e("MainActivity", "왔슘당")
         setContentView(binding.root)
 
         binding.startButton.setOnClickListener {
+            Log.e("켜지고 나서 흐른 시간", SystemClock.elapsedRealtime().toString() + "ㅎㅎ")
+            Log.e("stop 을 누르고 저장된 시간", pauseTime.toString() + "ㅎㅎ")
             binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
             binding.chronometer.start()
             // 버튼 표시 여부 조정
@@ -33,7 +37,9 @@ class StopWatchActivity : AppCompatActivity() {
         }
 
         binding.stopButton.setOnClickListener {
+            Log.e("켜지고 나서 흐른 시간", SystemClock.elapsedRealtime().toString() + "ㅎㅎ")
             pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
+            Log.e("stop 을 누르고 저장된 시간", pauseTime.toString() + "ㅎㅎ")
             binding.chronometer.stop()
             binding.stopButton.isEnabled = false
             binding.resetButton.isEnabled = true
@@ -42,6 +48,7 @@ class StopWatchActivity : AppCompatActivity() {
 
         binding.resetButton.setOnClickListener {
             pauseTime = 0L
+            Log.e("켜지고 나서 흐른 시간", SystemClock.elapsedRealtime().toString() + "ㅎㅎ")
             binding.chronometer.base = SystemClock.elapsedRealtime()
             binding.chronometer.stop()
             binding.stopButton.isEnabled = false
