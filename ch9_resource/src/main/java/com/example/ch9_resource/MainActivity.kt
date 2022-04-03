1ef84a3807b4f8042bc1bc22553e8be421aad97b
zkzkzhzj <zkzkzhzj@gmail.com>
2022-04-04 07:38:47 +0900

    test

--- ch9_resource/src/main/java/com/example/ch9_resource/MainActivity.kt
+++ ch9_resource/src/main/java/com/example/ch9_resource/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resources.getBoolean(R.bool.test_bool)
+        resources.getInteger(R.integer.test_integer)
+        resources.getString(R.string.app_name)
     }
 }
